e57bd929e39f426e3ed52f17bb7f8b98cb39e268
maatrasikk <sin2x.cos4x.tan2x@gmail.com>
2025-09-24 11:16:05 +0200

    panel works, sliders not

--- scripts/pc.gd
+++ scripts/pc.gd
@@ -1,28 +1,13 @@
 extends Area2D
 
-@export var panel_path: NodePath   # Укажешь в инспекторе
-
-func _ready():
-	print("[PC] ready. panel_path =", panel_path)
+@export var panel_path: NodePath  # Укажи панель в инспекторе
 
 func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
-		if panel_path == NodePath():
-			push_error("[PC] panel_path НЕ задан в инспекторе.")
-			return
 		var panel = get_node_or_null(panel_path)
 		if panel == null:
-			push_error("[PC] get_node(panel_path) вернул null. Текущий panel_path=" + str(panel_path))
 			return
-		print("[PC] panel=", panel, 
-			  " open=", panel.has_method("open"),
-			  " open_at_world=", panel.has_method("open_at_world"),
-			  " script=", panel.get_script())
 		if panel.has_method("open_at_world"):
-			print("[PC] Calling open_at_world")
 			panel.call("open_at_world", global_position)
 		elif panel.has_method("open"):
-			print("[PC] Calling open")
 			panel.call("open")
-		else:
-			push_error("Panel at path has no methods (open / open_at_world).")
